b1c7531e590a58a1bb53485b169f804dc0d7730a
Wanni <wannidev0928@gmail.com>
2024-09-19 00:32:34 +0900

    사용하려는 api의 종류에 따라 외부 리포지토리에 공개하는 것은 보안 취약점을 일으킬 수 있으므로 요청을 수행하는데 필요한 요소는 외부 properties 파일을 가져오도록 함

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,5 +1,86 @@
 package org.http.api
 
+import org.apache.hc.client5.http.classic.methods.HttpGet
+import org.apache.hc.client5.http.impl.classic.HttpClients
+import org.apache.hc.core5.http.ParseException
+import java.io.IOException
+import java.nio.file.Paths
+import java.util.Properties
+
 fun main() {
-    println("Hello World!")
+    val path = "${System.getProperty("user.dir")}/src/main/resources/application.properties"
+    var properties = mapOf<String, Any>()
+    Properties()
+        .apply {
+            load(Paths.get(path).toFile().reader())
+        }.forEach { (key, value) ->
+            val splitKey = key.toString().split(".")
+            val mutableProperties = properties.toMutableMap()
+            if (splitKey.size > 1) {
+                val depth1Key = splitKey[0]
+                mutableProperties.putIfAbsent(depth1Key, mapOf<String, String>())
+
+                val depth2Properties = mutableProperties[depth1Key] as Map<*, *>
+                val mutableDepth2Properties = depth2Properties.toMutableMap()
+                mutableDepth2Properties.putIfAbsent(splitKey[1], value.toString())
+
+                mutableProperties[depth1Key] = mutableDepth2Properties.toMap()
+            } else {
+                mutableProperties.putIfAbsent(key.toString(), value.toString())
+            }
+            properties = mutableProperties.toMap()
+        }
+
+    val apiEndPointUri =
+        ApiEndPointUri(
+            schema = properties["schema"].toString(),
+            host = properties["host"].toString(),
+        )
+    apiEndPointUri.appendPath(properties["path"].toString())
+
+    properties["parameters"]?.let {
+        val parameters = it as Map<*, *>
+        parameters.forEach { (key, value) ->
+            apiEndPointUri.appendParameter(ApiParameter(key.toString(), value.toString()))
+        }
+    }
+
+    properties["headers"]?.let {
+        val headers = it as Map<*, *>
+        headers.forEach { (key, value) ->
+            apiEndPointUri.appendHeader(ApiHeader(key.toString(), value.toString()))
+        }
+    }
+
+    val endPoint = apiEndPointUri.endPoint()
+    println(endPoint)
+    val headers = apiEndPointUri.headers()
+    println(headers)
+    val parameters = apiEndPointUri.parameters()
+    println(parameters)
+
+    try {
+        val httpGet = HttpGet(endPoint)
+        for (header in headers) {
+            httpGet.addHeader(header.name, header.value)
+        }
+        val httpClient = HttpClients.createDefault()
+        httpClient.execute(httpGet) { response ->
+            val version = response.version
+            val status = response.code
+            val message = response.reasonPhrase
+            val entities =
+                response.entity.content
+                    .readAllBytes()
+                    .decodeToString()
+            println("version: $version")
+            println("status: $status")
+            println("message: $message")
+            println("entities: $entities")
+        }
+    } catch (e: IOException) {
+        e.printStackTrace()
+    } catch (e: ParseException) {
+        e.printStackTrace()
+    }
 }
